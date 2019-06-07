6c566d6fa2277f472185cb50781a644e7dc21a2d
sonohoshi <yd111544@gmail.com>
2019-06-07 21:02:47 +0900

    Update queue.cpp
    4월달에 구현한채로 에러가 났던 큐를 수정한 것이다.

--- queue.cpp
+++ queue.cpp
@@ -2,67 +2,52 @@
 #include <string>
 using namespace std;
 
-int frontPtr = 0;
-int backPtr = 0;
-
-int queue[10000] = {0};
+int queue[10000] = { 0 };
+int frontPtr=0, backPtr=0;
 
 void push(int data) {
-	queue[frontPtr++] = data;
+	queue[backPtr++] = data;
 }
 
 int pop() {
-	if (!queue[frontPtr] && !queue[backPtr]) {
-		return -1;
-	}
-	int buffer = queue[frontPtr];
-	queue[frontPtr--] = 0;
+	int buffer = 0;
+	if (!(queue[frontPtr] + queue[backPtr])) return -1;
+	buffer = queue[frontPtr];
+	queue[frontPtr++] = 0;
 	return buffer;
 }
 
 int size() {
-	return frontPtr - backPtr ;
+	return backPtr - frontPtr;
 }
 
 bool empty() {
-	return !(queue[frontPtr] + queue[backPtr]) ? 1 : 0;
+	return !(queue[frontPtr] + queue[backPtr]);
 }
 
 int front() {
-	return !queue[frontPtr] ? -1 : queue[frontPtr];
+	if (!(queue[frontPtr] + queue[backPtr])) return -1;
+	return queue[frontPtr];
 }
 
 int back() {
-	return !queue[frontPtr] ? -1 : queue[backPtr];
+	if (!(queue[frontPtr] + queue[backPtr])) return -1;
+	return queue[backPtr-1];
 }
 
 int main() {
 	string str;
-	int num, n;
-	cin >> num;
-	for (int i = 0; i < num; i++) {
+	int n, num;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
 		cin >> str;
-		if (str == "push") {
-			cin >> n;
-			push(n);
-		}
-		else if (str == "pop") {
-			cout << pop() << endl;
-		}
-		else if (str == "size") {
-			cout << size() << endl;
-		}
-		else if (str == "empty") {
-			cout << empty() << endl;
-		}
-		else if (str == "front") {
-			cout << front() << endl;
-		}
-		else if (str == "back") {
-			cout << back() << endl;
-		}
+		if (str == "push") { cin >> num; push(num); }
+		else if (str == "pop") { cout << pop() << endl; }
+		else if (str == "size") { cout << size() << endl; }
+		else if (str == "empty") { cout << empty() << endl; }
+		else if (str == "front") { cout << front() << endl; }
+		else if (str == "back") { cout << back() << endl; }
 	}
-
 	system("pause");
 	return 0;
 }
