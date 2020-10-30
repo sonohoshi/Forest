e67922f394bcdb4f6ea45cc1d0e55a028d557c55
sonohoshi <yd111544@gmail.com>
2020-10-30 21:54:08 +0900

    Fix array graph and Add description

--- DataStruct/SimpleGraphByArray.cpp
+++ DataStruct/SimpleGraphByArray.cpp
@@ -2,22 +2,29 @@
 using namespace std;
 
 int main() {
+	// n = 정점의 개수, m = 간선의 개수
 	int n, m;
 	cin >> n >> m;
+
+	// n개의 각 정점마다 타 정점으로 갈 수 있도록 행렬을 생성
 	int** graph = new int* [n];
 	for (int i = 0; i < n; i++) {
-		graph[i] = new int[m];
+		graph[i] = new int[n];
 	}
 
+	// 각 간선을 -1로 초기화. 못가는 길은 0으로 더 많이 초기화 하는 것 같지만 아무튼.
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			graph[i][j] = -1;
 		}
 	}
 
-	int u, v;
-	cin >> u >> v;
-	graph[v][u] = graph[u][v] = 1;
+	// 간선의 수만큼 입력을 받아 초기화를 해줌.
+	for (int i = 0; i < m; i++) {
+		int u, v;
+		cin >> u >> v;
+		graph[v][u] = graph[u][v] = 1;
+	}
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
