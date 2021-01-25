9207ce060804d795a76ecfce4fba6201ff774e67
sonohoshi <yd111544@gmail.com>
2021-01-25 23:22:09 +0900

    Learn Binary tree generate maze algorithm
    x/y 둘 중 하나라도 짝수인 칸를 다 벽으로 막은 후, 이동 가능한 칸에서 오른쪽 또는 아래 둘 중 하나만 뚫는 방식의 미로.

---
+++ GameAlgorithm/Algorithm/Algorithm/Board.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Algorithm
+{
+    public class Board
+    {
+        private const char circle = '\u25cf';
+        private static Random rand = new Random();
+        private enum TileType : int
+        {
+            Empty = 0,
+            Wall = 1
+        }
+
+        public int[,] tile;
+        public int size;
+
+        public Board(int size)
+        {
+            if (size % 2 == 0)
+            {
+                throw new Exception("Size can not even number.");
+            }
+            
+            tile = new int[size, size];
+            this.size = size;
+
+            GenerateMazeByBinaryTree();
+        }
+
+        public void Render()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+            
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.ForegroundColor = GetTileColor((TileType) tile[j, i]);
+                    Console.Write(circle);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = prevColor;
+        }
+
+        private void GenerateMazeByBinaryTree()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (j % 2 == 0 || i % 2 == 0)
+                        tile[j, i] = (int) TileType.Wall;
+                    
+                    else
+                        tile[j, i] = (int) TileType.Empty;
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if ((j % 2 == 0 || i % 2 == 0) || (j == size - 2 && i == j))
+                        continue;
+
+                    if (j == size - 2)
+                    {
+                        tile[j, i + 1] = (int) TileType.Empty;
+                        continue;
+                    }
+                    
+                    if (i == size - 2)
+                    {
+                        tile[j + 1, i] = (int) TileType.Empty;
+                        continue;
+                    }
+
+                    if (GetRandomBoolean())
+                    {
+                        tile[j, i + 1] = (int) TileType.Empty;
+                    }
+                    else
+                    {
+                        tile[j + 1, i] = (int) TileType.Empty;
+                    }
+                }
+            }
+        }
+
+        private ConsoleColor GetTileColor(TileType type)
+        {
+            switch (type)
+            {
+                case TileType.Empty: return ConsoleColor.Cyan;
+                case TileType.Wall: return ConsoleColor.Red;
+                
+                default: return ConsoleColor.White;
+            }
+        }
+
+        private bool GetRandomBoolean()
+        {
+            return rand.NextDouble() < 0.5;
+        }
+    }
+}
---
+++ GameAlgorithm/Algorithm/Algorithm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Algorithm
+{
+  internal class Program
+  {
+    private const int waitTick = 1000 / 30;
+    
+    public static void Main(string[] args)
+    {
+      Console.CursorVisible = false;
+      Board board = new Board(25);
+
+      int lastTick = 0;
+      
+      while (true)
+      {
+        #region Frame Management
+        int curTick = System.Environment.TickCount;
+        if(curTick - lastTick < waitTick) continue;
+        lastTick = curTick;
+        #endregion
+
+        Console.SetCursorPosition(0, 0);
+        board.Render();
+      }
+    }
+  }
+}
