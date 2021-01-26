e15fd6228462de1ddebd3a4b4e0127625cb0766a
sonohoshi <yd111544@gmail.com>
2021-01-27 00:03:55 +0900

    Learn SideWinder generate maze algorithm
    Binary tree 미로 생성 알고리즘과 같은 전처리를 한 후 비슷한 로직으로 작동하는 알고리즘이지만, 오른쪽으로 쭉 뚫다가 아래쪽을 뚫게 되면 현재까지 오른쪽으로 연속해서 뚫던 칸 중 하나를 골라서 그 중 하나를 아래로 뚫는다.

--- GameAlgorithm/Algorithm/Algorithm/Board.cs
+++ GameAlgorithm/Algorithm/Algorithm/Board.cs
@@ -25,7 +25,8 @@ namespace Algorithm
             tile = new int[size, size];
             this.size = size;
 
-            GenerateMazeByBinaryTree();
+            //GenerateMazeByBinaryTree();
+            GenerateMazeBySideWinder();
         }
 
         public void Render()
@@ -45,6 +46,55 @@ namespace Algorithm
             Console.ForegroundColor = prevColor;
         }
 
+        private void GenerateMazeBySideWinder()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (j % 2 == 0 || i % 2 == 0)
+                        tile[i, j] = (int) TileType.Wall;
+                    
+                    else
+                        tile[i, j] = (int) TileType.Empty;
+                }
+            }
+            
+            for (int i = 0; i < size; i++)
+            {
+                int cnt = 1;
+                for (int j = 0; j < size; j++)
+                {
+                    if ((j % 2 == 0 || i % 2 == 0) || (i == size - 2 && i == j))
+                        continue;
+                    
+                    if (i == size - 2)
+                    {
+                        tile[i, j + 1] = (int) TileType.Empty;
+                        continue;
+                    }
+                    
+                    if (j == size - 2)
+                    {
+                        tile[i + 1, j] = (int) TileType.Empty;
+                        continue;
+                    }
+
+                    if (GetRandomBoolean())
+                    {
+                        tile[i, j + 1] = (int) TileType.Empty;
+                        cnt++;
+                    }
+                    else
+                    {
+                        int randIndex = rand.Next(0, cnt);
+                        tile[i + 1, j - randIndex] = (int) TileType.Empty;
+                        cnt = 1;
+                    }
+                }
+            }
+        }
+
         private void GenerateMazeByBinaryTree()
         {
             for (int i = 0; i < size; i++)
@@ -58,14 +108,13 @@ namespace Algorithm
                         tile[j, i] = (int) TileType.Empty;
                 }
             }
-
+            
             for (int i = 0; i < size; i++)
             {
                 for (int j = 0; j < size; j++)
                 {
                     if ((j % 2 == 0 || i % 2 == 0) || (j == size - 2 && i == j))
                         continue;
-
                     if (j == size - 2)
                     {
                         tile[j, i + 1] = (int) TileType.Empty;
