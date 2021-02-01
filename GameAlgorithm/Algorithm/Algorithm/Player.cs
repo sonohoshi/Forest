68fdf2a5435f11d22680628cbb022f86f75f7f0a
sonohoshi <yd111544@gmail.com>
2021-02-02 01:30:08 +0900

    Learn about Right hand rule
    Maze solver

--- GameAlgorithm/Algorithm/Algorithm/Player.cs
+++ GameAlgorithm/Algorithm/Algorithm/Player.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
 
 namespace Algorithm
 {
+    class Position
+    {
+        public int x, y;
+        
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
     public class Player
     {
+        private enum Direction
+        {
+            Up = 0,
+            Left = 1,
+            Right = 2,
+            Down = 3,
+        }
+        
         private static Random rand;
         
         private Board board;
+        private int dir = (int) Direction.Up;
+        private List<Position> positions = new List<Position>();
         
         public int x { get; private set; }
         public int y { get; private set; }
@@ -22,46 +45,54 @@ namespace Algorithm
                 rand = new Random();
             }
 
+            int[] frontY = new[] {-1, 0, 1, 0};
+            int[] frontX = new[] {0, -1, 0, 1};
+            int[] rightY = new[] {0, -1, 0, 1};
+            int[] rightX = new[] {1, 0, -1, 0};
+
+            positions.Add(new Position(x, y));
             
+            while (x != board.DestX || y != board.DestY)
+            {
+                if (board.Tile[y + rightY[dir], x + rightX[dir]] == (int) Board.TileType.Empty)
+                {
+                    dir = (dir - 1 + 4) % 4;
+                    x += frontX[dir];
+                    y += frontY[dir];
+                    
+                    positions.Add(new Position(x, y));
+                }
+                else if(board.Tile[y + frontY[dir], x + frontX[dir]] == (int) Board.TileType.Empty)
+                {
+                    x += frontX[dir];
+                    y += frontY[dir];
+                    
+                    positions.Add(new Position(x, y));
+                }
+                else
+                {
+                    dir = (dir + 1 + 4) % 4;
+                }
+            }
         }
         
         private const int moveTick = 100;
         private int sumTick = 0;
+        private int lastIndex = 0;
         public void Update(int deltaTick)
         {
+            if (lastIndex >= positions.Count) return;
+            
             sumTick += deltaTick;
             if (sumTick >= moveTick)
             {
                 sumTick = 0;
 
                 int randValue = rand.Next(0, 4);
-                switch (randValue)
-                {
-                    case 0:
-                        if (y - 1 >= 0 && board.Tile[y - 1, x] == (int) Board.TileType.Empty)
-                        {
-                            y -= 1;
-                        }
-                        break;
-                    case 1:
-                        if (y + 1 < board.Size - 1 && board.Tile[y + 1, x] == (int) Board.TileType.Empty)
-                        {
-                            y += 1;
-                        }
-                        break;
-                    case 2:
-                        if (x - 1 >= 0 && board.Tile[y, x - 1] == (int) Board.TileType.Empty)
-                        {
-                            x -= 1;
-                        }
-                        break;
-                    case 3:
-                        if (x + 1 < board.Size - 1 && board.Tile[y, x + 1] == (int) Board.TileType.Empty)
-                        {
-                            x += 1;
-                        }
-                        break;
-                }
+                y = positions[lastIndex].y;
+                x = positions[lastIndex].x;
+
+                lastIndex++;
             }
         }
     }
