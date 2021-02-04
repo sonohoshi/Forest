aa6f9881e39161121281c3739aa580bbf81100b8
sonohoshi <yd111544@gmail.com>
2021-02-04 21:16:33 +0900

    Learn about DFS
    Depth First Search

--- GameAlgorithm/Algorithm/DataStructure/Program.cs
+++ GameAlgorithm/Algorithm/DataStructure/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DataStructure
 {
@@ -25,11 +27,46 @@ namespace DataStructure
                 new List<int>() {3, 5},
                 new List<int>() {4}
             };
+
+            bool[] visited = new bool[6];
+            public void DFS(int start)
+            {
+                Console.WriteLine(start);
+                visited[start] = true;
+
+                for (int i = 0; i < adj.GetLength(0); i++)
+                {
+                    // 연결되어있지 않거나 방문한 노드라면 처리 안함.
+                    if(adj[start, i] == 0 || visited[i]) continue;
+                    DFS(i);
+                }
+            }
+
+            public void DFSWithList(int start)
+            {
+                Console.WriteLine(start);
+                visited[start] = true;
+
+                foreach (var node in adj2[start].Where(node => !visited[node]))
+                {
+                    DFSWithList(node);
+                }
+            }
+
+            public void SearchAllWithDFS()
+            {
+                visited = new bool[6];
+                for (int i = 0; i < adj.GetLength(0); i++)
+                {
+                    if(!visited[i]) DFS(i);
+                }
+            }
         }
         
         public static void Main(string[] args)
         {
-            
+            Graph graph = new Graph();
+            graph.SearchAllWithDFS();
         }
     }
 }
