84701273bcd3390ae5bd8b92d50e64a753d0ee36
sonohoshi <yd111544@gmail.com>
2021-02-07 03:29:45 +0900

    Learn about Dijikstra algorithm
    fuck yeah

--- GameAlgorithm/Algorithm/DataStructure/Program.cs
+++ GameAlgorithm/Algorithm/DataStructure/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,12 +11,12 @@ namespace DataStructure
         {
             private int[,] adj = new int[6, 6]
             {
-                {0, 1, 0, 1, 0, 0},
-                {1, 0, 1, 1, 0, 0},
-                {0, 1, 0, 0, 0, 0},
-                {1, 1, 0, 0, 1, 0},
-                {0, 0, 0, 1, 0, 1},
-                {0, 0, 0, 0, 1, 0}
+                {0, 15, 0, 35, 0, 0},
+                {15, 0, 5, 10, 0, 0},
+                {0, 5, 0, 0, 0, 0},
+                {35, 10, 0, 0, 5, 0},
+                {0, 0, 0, 5, 0, 5},
+                {0, 0, 0, 0, 5, 0}
             };
 
             private List<int>[] adj2 = new List<int>[]
@@ -91,12 +92,56 @@ namespace DataStructure
                     }
                 }
             }
+
+            public void Dijikstra(int start)
+            {
+                bool[] visited = new bool[6];
+                int[] distance = new int[6];
+                int[] parent = new int[6];
+
+                for (int i = 0; i < distance.Length; i++) distance[i] = int.MaxValue;
+
+                distance[start] = 0;
+                parent[start] = start;
+                
+                while (true)
+                {
+                    int closest = int.MaxValue;
+                    int now = -1;
+                    for (int i = 0; i < distance.Length; i++)
+                    {
+                        // 이미 방문했거나, 거리 체크조차도 안됐거나, 기존 후보보다 멀리있으면 스킵.
+                        if(visited[i] || distance[i] == int.MaxValue || distance[i] >= closest) continue;
+                        
+                        closest = distance[i];
+                        now = i;
+                    }
+
+                    // 다음 후보가 없다. 즉, 연결이 단절됐거나 이미 모든 곳을 방문했다.
+                    if (now == -1) break;
+
+                    visited[now] = true;
+
+                    for (int next = 0; next < distance.Length; next++)
+                    {
+                        if(adj[now, next] == 0 || visited[next]) continue;
+
+                        int nextDistance = distance[now] + adj[now, next];
+                        if (distance[next] > nextDistance)
+                        {
+                            distance[next] = nextDistance;
+                            parent[next] = now;
+                        }
+                    }
+                }
+            }
         }
         
         public static void Main(string[] args)
         {
             Graph graph = new Graph();
             graph.BFS(0);
+            graph.Dijikstra(0);
         }
     }
 }
