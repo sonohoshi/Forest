4d50f6caed34904e238735b1432cdc3f1487ef48
sonohoshi <yd111544@gmail.com>
2021-03-15 23:45:47 +0900

    Implement Quick sort with C#

---
+++ DataStructAndAlgorithm/QuickSort.cs
@@ -0,0 +1,37 @@
+public static class Extensions
+{
+    private static int DivideArray(int[] arr, int left, int right)
+    {
+        var pivotValue = arr[left];
+        var leftIndex = left;
+        var rightIndex = right;
+
+        while (leftIndex < rightIndex)
+        {
+            if ((leftIndex <= right) && (arr[leftIndex] < pivotValue)) leftIndex++;
+            if ((rightIndex >= left) && (arr[rightIndex] > pivotValue)) rightIndex--;
+
+            if (leftIndex < rightIndex)
+            {
+                var temp = arr[leftIndex];
+                arr[leftIndex] = arr[rightIndex];
+                arr[rightIndex] = temp;
+
+                if (arr[leftIndex] == arr[rightIndex]) rightIndex--;
+            }
+        }
+
+        return rightIndex;
+    }
+    
+    public static void QuickSort(this int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int pivot = DivideArray(arr, left, right);
+
+            arr.QuickSort(left, pivot - 1);
+            arr.QuickSort(pivot + 1, right);
+        }
+    }
+}
